e18e80a4d4accd00d1d9447995977ddaf80079c4
yoann-fermaud <yoann.fermaud@laplateforme.io>
2022-10-20 10:14:24 +0200

    Update ftp-install Job07

---
+++ Job07/ftpInstall.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "#############################"
+echo "#    Root user required     #"
+echo "#############################"
+
+echo "Option '1' Install and configure ProFTPd"
+echo "Option '2' Quit script"
+
+echo -n "Choose an option : "
+read option
+
+case $option in
+    "1")
+        apt -y install proftpd-* && apt-get -y update && apt-get -y upgrade
+        mv /etc/proftpd/proftpd.conf /etc/proftpd/proftpd_backup.conf
+        mv /etc/proftpd/tls.conf /etc/proftpd/tls_backup.conf
+        mv /etc/proftpd/modules.conf /etc/proftpd/modules_backup.conf
+        
+        git clone https://github.com/yoann-fermaud/FTP.git
+        mv FTP/ftp-config/ftp-config-proftpd.conf /etc/proftpd/proftpd.conf
+        mv FTP/ftp-config/ftp-config-tls.conf /etc/proftpd/tls.conf
+        mv FTP/ftp-config/ftp-config-modules.conf /etc/proftpd/modules.conf
+        rm -r ftp-config/
+        
+        mkdir -p /etc/proftpd/ssl
+	openssl req -x509 -nodes -days 365 -newkey rsa:4096 -out /etc/proftpd/ssl/proftpd-rsa.pem -keyout /etc/proftpd/ssl/proftpd-key.pem
+	#-subj "/C=''/ST=''/L=''/O=''/OU=''/CN=''" 
+        chmod 0600 /etc/proftpd/ssl/proftpd-key.pem
+        
+        service proftpd restart
+        echo "Configuration done !"
+    ;;
+    
+    "2")
+        exit
+    ;;
+    
+    *)
+        echo "Error : available option are '1', '2' or '3'"
+    ;;
+esac
